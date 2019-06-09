45fc764ed7e11d2a234ec1bb89be2702338d4cfd
karthik51 <karthikeyan51@gmail.com>
2019-06-09 22:55:49 +0530

    Add project files.

---
+++ ClientApp/src/app/upload/upload.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpRequest, HttpEventType, HttpResponse } from '@angular/common/http'
+
+@Component({
+  selector: 'app-upload',
+  templateUrl: './upload.component.html',
+  styleUrls: ['./upload.component.css']
+})
+export class UploadComponent implements OnInit {
+  
+  ngOnInit() {
+  }
+  public progress: number;
+  public message: string;
+  public uploadFiles : any;
+  constructor(private http: HttpClient) { }
+
+  upload(files) {
+    if (files.length === 0)
+      return;
+      this.uploadFiles = files;
+
+    const formData = new FormData();
+
+    for (let file of files)
+      formData.append(file.name, file);
+
+    const uploadReq = new HttpRequest('POST', 'api/upload', formData, {
+      reportProgress: true,
+    });
+
+    this.http.request(uploadReq).subscribe(event => {
+      if (event.type === HttpEventType.UploadProgress)
+        this.progress = Math.round(100 * event.loaded / event.total);
+      else if (event.type === HttpEventType.Response)
+        this.message = event.body.toString();
+    });
+  }
+
+}
